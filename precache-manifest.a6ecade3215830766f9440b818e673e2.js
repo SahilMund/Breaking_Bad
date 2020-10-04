9854abaa5ed31ca953963b2dba91b60ebf22d8e3
SahilMund <53625989+SahilMund@users.noreply.github.com>
2020-10-04 10:57:31 +0530

    Updates

---
+++ precache-manifest.a6ecade3215830766f9440b818e673e2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62f2f00a9c96c0234abdc48dd362be8e",
+    "url": "/Breaking_Bad/index.html"
+  },
+  {
+    "revision": "e74e850caf62b08c2ae3",
+    "url": "/Breaking_Bad/static/css/main.f272ee9b.chunk.css"
+  },
+  {
+    "revision": "b4322beda5e52d47f8ea",
+    "url": "/Breaking_Bad/static/js/2.c5c38f72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Breaking_Bad/static/js/2.c5c38f72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e74e850caf62b08c2ae3",
+    "url": "/Breaking_Bad/static/js/main.2fde59a7.chunk.js"
+  },
+  {
+    "revision": "ef7bd4f01a666651c16c",
+    "url": "/Breaking_Bad/static/js/runtime-main.222a7927.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/Breaking_Bad/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/Breaking_Bad/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/Breaking_Bad/static/media/spinner.235b9b3e.gif"
+  }
+]);
